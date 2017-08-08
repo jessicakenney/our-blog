c5f8998f504c01e5126f4fc2fc22b30988af9a78
Esti Shay and Jessica Sheridan <jessica@>
2017-08-08 09:24:04 -0700

    correct syntax for templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,14 +12,14 @@ public class App {
 
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, index.hbs);
+            return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
         post("/posts/new", (request, response) -> { //URL to make new post on POST route
             Map<String, Object> model = new HashMap<String, Object>();
             String content = request.queryParams("content");
             Post newPost = new Post(content);
-            return new ModelAndView(model, success.hbs);
+            return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
 
     }
