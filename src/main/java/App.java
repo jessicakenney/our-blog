efb236ad8ead4cad27a9dd994c542775325e19e1
Esti Shay and Jessica Sheridan <esti@>
2017-08-08 09:21:12 -0700

    add method to create new post and send user to success page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 import java.util.Map;
 import java.util.HashMap;
+
+import models.Post;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -11,6 +13,13 @@ public class App {
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, index.hbs);
+        }, new HandlebarsTemplateEngine());
+
+        post("/posts/new", (request, response) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<String, Object>();
+            String content = request.queryParams("content");
+            Post newPost = new Post(content);
+            return new ModelAndView(model, success.hbs);
         }, new HandlebarsTemplateEngine());
 
     }
