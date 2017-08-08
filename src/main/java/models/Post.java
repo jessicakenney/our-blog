66d39413023ccc7af6a9ca54186749c550e95b65
Esti Shay and Jessica Sheridan <jessica@>
2017-08-08 08:31:50 -0700

    create initial project

---
+++ src/main/java/models/Post.java
@@ -0,0 +1,9 @@
+package models;
+
+
+public class Post {
+
+    public Post (String content) {
+
+    }
+}
---
+++ src/test/java/models/PostTest.java
@@ -0,0 +1,24 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class PostTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void NewPostObjectGetsCorrectlyCreates_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals(true, post instanceof Post);
+    }
+}
